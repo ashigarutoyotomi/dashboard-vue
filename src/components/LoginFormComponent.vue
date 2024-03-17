5b72f62186e1473ff96f0124ad1d31e150515280
ashigarutoyotomi <abecodeman@gmail.com>
2024-03-17 17:56:21 +0600

    pages done

--- src/components/LoginFormComponent.vue
+++ src/components/LoginFormComponent.vue
@@ -1,10 +1,17 @@
 <template>
+  <h3 v-if="errors">{{ errors }}</h3>
   <el-form :model="form" label-width="auto" style="max-width: 600px">
-    <el-form-item label="Activity email">
-      <el-input v-model="form.email" />
+    <el-form-item label="EMail">
+      <el-input
+        v-model="form.email"
+        maxlength="30"
+        required
+        type="email"
+        value="admin@proton.com"
+      />
     </el-form-item>
-    <el-form-item label="Activity form">
-      <el-input v-model="form.password" type="textarea" />
+    <el-form-item label="Activity">
+      <el-input v-model="form.password" maxlength="50" type="password" required value="123" />
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="onSubmit">Create</el-button>
@@ -13,13 +20,42 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
-
+import { reactive, ref } from 'vue'
+import axios from 'axios'
+import router from '@/router'
 // do not use same email with ref
 const form = reactive({
-  email: '',
-  password: ''
+  email: 'admin@proton.com',
+  password: '123',
+  user: {}
 })
+const errors = ref({})
+const onSubmit = async () => {
+  try {
+    const response = await axios
+      .post(import.meta.env.VITE_API_URL + '/login', {
+        email: form.email,
+        password: form.password
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          // alert(response.data.token)
+          axios.defaults.headers.common['Authorization'] = `"Bearer ${response.data.token}"`
+          form.user = response.data.user
+          router.push('/')
+        } else {
+          return false
+        }
+      })
+  } catch (e) {
+    console.log(e)
+    if (e.response.status === 422) {
+      errors.value = e.response['data']['message']
+      return false
+    }
 
-const onSubmit = () => {}
+    form.email = ''
+    form.password = ''
+  }
+}
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import './assets/main.css'
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
-
+import 'element-plus/dist/index.css'
 const app = createApp(App)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
