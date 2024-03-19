fec219d506d8d08a729b57eed98ea81ba49b07ee
ashigarutoyotomi <abecodeman@gmail.com>
2024-03-19 08:14:51 +0600

    guide done

--- src/store/users/index.ts
+++ src/store/users/index.ts
@@ -3,21 +3,21 @@ import { ref } from 'vue'
 import { User } from '../../services/users/types'
 
 export const useUserStore = defineStore('userStore', () => {
-  const users = ref<User[]>([])
+  const user = ref<User[]>([])
 
-  function initUsers(data: User[]) {
-    users.value = data
+  function initUser(data: User[]) {
+    user.value = data
   }
-
-  function removeUser(id: number) {
-    const idx = users.value.findIndex((s) => s.id === id)
-    if (idx === -1) return
-    users.value.splice(idx, 1)
+  function flushUser() {
+    user.value = 
   }
-
+  // function removeUser(id: number) {
+  //   const idx = user.value.findIndex((s) => s.id === id)
+  //   if (idx === -1) return
+  //   user.value.splice(idx, 1)
+  // }
   return {
-    users,
-    initUsers,
-    removeUser
+    user,
+    initUser
   }
 })
